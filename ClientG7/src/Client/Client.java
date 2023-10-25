d2ed9dc9a3befa057750717935fc3bbc0691713a
Najahi <96703414+NajahiAhlam@users.noreply.github.com>
2023-10-25 16:18:58 +0100

    Initial commit

---
+++ ClientG7/src/Client/Client.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Client;
+
+import dao.IDao;
+import entities.Machine;
+import entities.Salle;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import utils.Config;
+
+/**
+ *
+ * @author Lachgar
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        try {
+           IDao<Machine> ms = (IDao<Machine>) Naming.lookup("rmi://localhost:1998/machine");
+           IDao<Machine> ss = (IDao<Machine>) Naming.lookup("rmi://localhost:1998/salle");
+
+ ;
+
+            for (Machine m : ms.findAll()) {
+                System.out.println(m);
+            }
+
+        } catch (NotBoundException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (RemoteException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
